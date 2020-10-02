7705d372e02597f76b048acdeaff21a528608bda
PinkWays <helloitpdu@gmail.com>
2020-10-02 09:12:37 +0700

    phuong test api heroku

--- web/api/a1toolgen/Controller/SelectAllByWhat.php
+++ web/api/a1toolgen/Controller/SelectAllByWhat.php
@@ -1,8 +1,9 @@
 <?php
-	header("Access-Control-Allow-Origin: http://localhost:4200");
-	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-	header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization, X-Auth-Token');
-	header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS');
+	header('Access-Control-Allow-Origin: *'); 
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token , Authorization');
 	header('Content-Type: text/html; charset=utf-8'); 
 	include "BaseConnection.php";
 	$data = json_decode(file_get_contents("php://input"));
